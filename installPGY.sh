daa3f07f9b766ddca4c7ba4c6366238ffdffeef7
Ryan Lieu <github-benzBrake@woai.ru>
2020-07-12 09:45:23 +0800

    try to fix aarch64 build

--- installPGY.sh
+++ installPGY.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ARCH=$(uname -m)
-if [[ $ARCH =~ "arm" ]]; then
+if [[ $ARCH =~ "arm" ]] || [[ $ARCH =~ "aarch" ]]; then
     DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=58"
     curl -SL "${DOWNLOAD_LINK}" -o /tmp/pygvpn.deb
     dpkg -i /tmp/pgyvpn.deb --force
     rm -f /tmp/pygvpn.deb
-else
+else 
     if [[ $ARCH =~ "386" ]]; then
         DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=42&os=Redhat / CentOS Linux(i386)"
     elif [[ $ARCH =~ "x86_64" ]]; then
