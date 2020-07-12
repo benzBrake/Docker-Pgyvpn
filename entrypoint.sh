814ab470b6b34b1c5b4a29012c46a8dfba76a136
Ryan Lieu <github-benzBrake@woai.ru>
2020-07-12 09:36:17 +0800

    fix entrypoint.sh not found fix image name mistake

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+/usr/bin/expect <<EOF
+	spawn /usr/sbin/pgyvpn
+	expect "Please Enter Your Account:"
+	send "${PGY_USERNAME}\r"
+	expect "Please Enter Your Password:"
+	send "${PGY_PASSWORD}\r"
+	expect "Auto Login"
+	send "n\r"
+	expect "Login Successful"
+	interact
+EOF
+/usr/bin/top -b
