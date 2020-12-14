fc6b2cf08f27614c3bd1bbfa49f0c1f098ddd371
Ryan Lieu <github-benzBrake@woai.ru>
2020-12-14 21:54:23 +0800

    try to fix for aarch64

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,10 @@ FROM benzbrake/alpine
 MAINTAINER Ryan Lieu <github-benzBrake@woai.ru>
 
 ENV PGY_USERNAME PGY_PASSOWRD
-ADD entrypoint.sh /data/
-ADD installPGY.sh /data/
+ADD installPGY.sh /
 RUN \
 	set -ex && \
-	apk --update --no-cache add bash expect rpm libc6-compat libgcc libstdc++ dpkg && \
-	chmod +x /data/*.sh && \
-	/data/installPGY.sh && \
-	apk del rpm bash dpkg
-ENTRYPOINT ["/data/entrypoint.sh"]
+	apk --update --no-cache add bash expect rpm libc6-compat libgcc libstdc++ musl dpkg && \
+	chmod +x /*.sh && \
+	/installPGY.sh
+ENTRYPOINT ["/usr/share/pgyvpn/script/pgyvpn_monitor"]
--- installPGY.sh
+++ installPGY.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 ARCH=$(uname -m)
 if [[ $ARCH =~ "arm" ]] || [[ $ARCH =~ "aarch" ]]; then
-    DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=58"
-    curl -SL "${DOWNLOAD_LINK}" -o /tmp/pygvpn.deb
-    dpkg -i /tmp/pgyvpn.deb --force
-    rm -f /tmp/pygvpn.deb
-else 
+    if [[ $ARCH =~ "64" ]]; then
+        DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=58&os=Raspberry Pi 64位"
+    else
+        DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=58&os=Raspberry Pi 32位"
+    fi
+    wget "${DOWNLOAD_LINK}" -O /tmp/pgyvpn.deb
+    dpkg --force-all -i /tmp/pgyvpn.deb
+    rm -f /tmp/pgyvpn.deb
+else
     if [[ $ARCH =~ "386" ]]; then
         DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=42&os=Redhat / CentOS Linux(i386)"
     elif [[ $ARCH =~ "x86_64" ]]; then
@@ -14,7 +18,7 @@ else
         echo "Do not support your system arch:"$(uname -m)
         exit 1
     fi
-    curl -SL "${DOWNLOAD_LINK}" -o /tmp/pygvpn.rpm
-    rpm --nodeps -i /tmp/pygvpn.rpm
-    rm -f /tmp/pygvpn.rpm
+    wget "${DOWNLOAD_LINK}" -O /tmp/pgyvpn.rpm
+    rpm --nodeps -i /tmp/pgyvpn.rpm
+    rm -f /tmp/pgyvpn.rpm
 fi
