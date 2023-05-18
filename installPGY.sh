2a29708baf1785dec5be6211dcccd4a40251d434
Ryan Lieu <github-benzBrake@woai.ru>
2023-05-18 11:40:47 +0800

    fix： 安装包下载地址变更导致无法安装新版

--- installPGY.sh
+++ installPGY.sh
@@ -2,18 +2,18 @@
 ARCH=$(uname -m)
 if [[ $ARCH =~ "arm" ]] || [[ $ARCH =~ "aarch" ]]; then
     if [[ $ARCH =~ "64" ]]; then
-        DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=58&os=Raspberry Pi 64位"
+        DOWNLOAD_LINK="https://pgy.oray.com/softwares/58/download/1839/PgyVisitor_Raspberry_2.4.0.52291_arm64.deb"
     else
-        DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=58&os=Raspberry Pi 32位"
+        DOWNLOAD_LINK="https://pgy.oray.com/softwares/58/download/1839/PgyVisitor_Raspberry_2.4.0.52291_armhf.deb"
     fi
     wget "${DOWNLOAD_LINK}" -O /tmp/pgyvpn.deb
     dpkg --force-all -i /tmp/pgyvpn.deb
     rm -f /tmp/pgyvpn.deb
 else
     if [[ $ARCH =~ "386" ]]; then
-        DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=42&os=Redhat / CentOS Linux(i386)"
+        DOWNLOAD_LINK="https://pgy.oray.com/softwares/42/download/1838/PgyVisitor_Ubuntu_2.4.0.52291_i386.deb"
     elif [[ $ARCH =~ "x86_64" ]]; then
-        DOWNLOAD_LINK="https://pgy.oray.com/package/dl?id=42&os=Redhat / CentOS Linux(X86_64)"
+        DOWNLOAD_LINK="https://pgy.oray.com/softwares/42/download/1838/PgyVisitor_Ubuntu_2.4.0.52291_x86_64.deb"
     else
         echo "Do not support your system arch:"$(uname -m)
         exit 1
